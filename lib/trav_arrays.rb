d2e415a99c4a176aa03fe0dabb07c9fdc8f54369
Leam Hall <leamhall@gmail.com>
2014-11-27 08:56:56 -0500

    Added Marine Officer options.

--- lib/trav_arrays.rb
+++ lib/trav_arrays.rb
@@ -3,7 +3,7 @@
 
 Grade = Hash.new
 Grade['Enlisted'] = %w(E1 E2 E3 E4 E5 E6 E7 E8 E9)
-
+Grade['Officer'] = %w(O1 O2 O3 O4 O5 O6)
 Ranks = Hash.new
 
 Ranks['Marines'] = {
@@ -14,7 +14,13 @@ Ranks['Marines'] = {
   'E5' => 'Sergeant',
   'E6' => 'Leading Sergeant',
   'E7' => 'First Sergeant',
-  'E8' => 'Sergeant Major'
+  'E8' => 'Sergeant Major',
+  'O1' => 'Lieutenant',
+  'O2' => 'Captain',
+  'O3' => 'Force Commander',
+  'O4' => 'Lieutenant Colonel',
+  'O5' => 'Colonel',
+  'O6' => 'Brigadier'
 }
 
 
