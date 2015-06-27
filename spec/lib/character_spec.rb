ebebbc6ff35762d36ebf4ea47c17361a8ed2c7d9
Leam Hall <leamhall@gmail.com>
2015-06-27 09:28:32 -0400

    Changed 'should' to 'expect().to' style in character_spec.rb.

--- spec/lib/character_spec.rb
+++ spec/lib/character_spec.rb
@@ -2,13 +2,17 @@
 require 'character'
 
 describe Character do
-  it { should respond_to(:career) }
-  it { should respond_to(:gender) }
+  it 'has a career' do
+    expect(subject).to respond_to(:career) 
+  end
+  it 'has a gender' do
+    expect(subject).to respond_to(:gender) 
+  end
 
   describe '#skills' do
     context 'when no skills set' do
       it 'returns empty hash' do
-        subject.skills.count == 0
+        expect(subject.skills.count).to be == 0
       end
     end
     context 'when a skill first added' do
@@ -17,13 +21,13 @@ describe Character do
       before { subject.skills[skill] = skill_level }
 
       it 'returns a skill level of 1' do
-        subject.skills[skill] == skill_level
+        expect(subject.skills[skill]).to be == skill_level
       end
     end
     context 'when a skill is incrementd' do
       let(:skill) {'Engineering'}
       it 'returns nil when skill not yet added' do
-        subject.skills[skill] == nil
+        expect(subject.skills[skill]).to be == nil
       end
     end
 
@@ -39,18 +43,18 @@ describe Character do
       it 'returns +1 when using the default level value' do
         old_level = subject.skills[skill]
         subject.increase_skill(skill)
-        subject.skills[skill].should == old_level + default_level
+        expect(subject.skills[skill]).to be == old_level + default_level
       end
       it 'returns specifically increased value when non-default used.' do
         old_level = subject.skills[skill]
         subject.increase_skill(skill, high_level)
-        subject.skills[skill].should == old_level + high_level
+        expect(subject.skills[skill]).to be == old_level + high_level
       end
       it 'returns combination of default and higher skills.' do
         subject.increase_skill(skill)
         initial_level = subject.skills[skill]
         subject.increase_skill(skill, high_level)
-        subject.skills[skill].should == default_level + high_level
+        expect(subject.skills[skill]).to be == default_level + high_level
       end
     end 
   
@@ -59,8 +63,8 @@ describe Character do
   describe '#age' do
     context 'when terms are not set' do
       it 'returns the default age' do
-        subject.age.should be >= Character::DEFAULT_AGE
-        subject.age.should be <= Character::DEFAULT_AGE + 3
+        expect(subject.age).to be >= Character::DEFAULT_AGE
+        expect(subject.age).to be <= Character::DEFAULT_AGE + 3
       end
     end
 
@@ -70,22 +74,16 @@ describe Character do
       before { subject.terms = terms }
 
       it 'calculates age based on terms' do
-        subject.age.should be >= 34
-        subject.age.should be <= 37
+        expect(subject.age).to be >= 34
+        expect(subject.age).to be <= 37
       end
     end
   end
 
   describe '#name' do
-    context 'when not set' do
-      it 'returns a blank string when name not set' do
-        subject.name.should == '' 
-      end
-    end
     context 'when set' do
-      before { subject.set_name }
       it 'returns the right name' do
-        subject.name.length.should >= 3
+        expect(subject.name.length).to be >= 4
       end
     end
   end
@@ -93,7 +91,7 @@ describe Character do
   describe '#terms' do
     context 'when not set' do
       it 'returns 0' do
-        subject.terms.should == 0
+        expect(subject.terms).to be 0
       end
     end
   end
@@ -101,19 +99,19 @@ describe Character do
   describe '#officer' do
     context 'when not set' do
       it 'returns false' do
-        subject.officer.should == false
+        expect(subject.officer).to be_falsey
       end
     end
     context 'when given an easy roll' do
       before { subject.officer(1) }
       it 'returns true' do
-        subject.officer.should == true
+        expect(subject.officer).to be_truthy
       end
     end
     context 'when given an impossible roll' do
       before { subject.officer(14) }
       it 'returns false' do
-        subject.officer.should == false
+        expect(subject.officer).to be_falsey
       end
     end
   end
@@ -121,13 +119,12 @@ describe Character do
   describe '#upp' do
     context 'when first created' do
       it 'returns a 6 character string' do
-        subject.upp.length.should == 6
+        expect(subject.upp.length).to be == 6
       end
       it 'is all hexidecimal characters' do
-        subject.upp.should match /[0-9A-F]{6}/
+        expect(subject.upp).to match(/[0-9A-F]{6}/)
       end
     end
   end
-
 
 end
