ce15aeb345876f13f29c768a6c13b0066d6a9e6c
Leam Hall <leamhall@gmail.com>
2015-07-26 18:38:50 -0400

    Moved character generation into a function.

--- chargen.rb
+++ chargen.rb
@@ -9,7 +9,19 @@ require 'optparse'
 require 'character'
 require 'Chargen'
 
-#options = { :career => nil }
+def gen_char(career)
+  # Need a failure mode if the files don't exist. 
+  name = career.capitalize
+  character = Chargen.const_get(name).new
+  character.career = name
+  character.terms = roll1
+  character.set_upp
+  character.set_rank
+  character.set_name(character.gender)
+  character.set_skills()
+  return character
+end
+
 career = ''
 
 parser = OptionParser.new do |opts|
@@ -20,26 +32,13 @@ parser = OptionParser.new do |opts|
   opts.on( '-c career', 'Select a career. Current options are:
                          Marine, Navy, Scout, Warder, College,
                          Guide, Entertainer' ) do |c|
-    #options[:career] = c
     career = c
   end
   
 end
 parser.parse!
 
-#career = options[:career]
-
-# Need a failure mode if the files don't exist. 
-name = career.capitalize
-character = Chargen.const_get(name).new
-character.career = name
-
-
-character.terms = roll1
-character.set_upp
-character.set_rank
-character.set_name(character.gender)
-character.set_skills()
+character = gen_char(career)
 
 ###  Output section
 out_txt(character)
