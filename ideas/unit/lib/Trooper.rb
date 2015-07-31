f0f5336a779072ef4060ec4c286e5ff6dce9e518
Leam Hall <leamhall@gmail.com>
2015-07-31 18:29:47 -0400

    Updated show_unit.rb

--- ideas/unit/lib/Trooper.rb
+++ ideas/unit/lib/Trooper.rb
@@ -1,16 +1,20 @@
 class Trooper
 
   def initialize(trooper)
-    this_trooper = Hash.new
-    this_trooper = trooper[1]
-    @rank = this_trooper['rank']
-    @first_name = this_trooper['first_name']
-    @last_name = this_trooper['last_name']
-    @morale = this_trooper['morale']
+#    this_trooper = Hash.new
+#    this_trooper = trooper[1]
+    @rank = trooper['rank']
+    @first_name = trooper['first_name']
+    @last_name = trooper['last_name']
+    @morale = trooper['morale']
   end 
 
-  def trooper_text()
-    puts "#{@rank} #{@first_name} #{@last_name}  #{@morale}"
+  def trooper_text
+    return "#{@rank} #{@first_name} #{@last_name}  #{@morale}"
+  end
+  
+  def trooper_morale?
+    return @morale
   end
 
 end
