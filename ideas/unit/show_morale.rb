9c96ebda99a0dcd2935e4ae680f39c31f7c03926
Leam Hall <leamhall@gmail.com>
2015-07-31 13:58:11 -0400

    Added a file to give a TO&E for the Blue Dragons.
    Uses JSON, and I need to write a script to add teams.

--- ideas/unit/show_morale.rb
+++ ideas/unit/show_morale.rb
@@ -1,18 +1,22 @@
 #!/usr/bin/env ruby
 
 require 'json'
+require 'pp'
 
 character_file = File.read('battle_results.json')
 unit = JSON.parse(character_file)
 
-#unit.each do |trooper|
-#  this_trooper = Hash.new
-#  this_trooper = trooper[1]
-#  print " #{this_trooper['rank']} #{this_trooper['first_name']} #{this_trooper['last_name']}  "
-#  puts "Morale #{this_trooper['morale']}"
-#end
+unit.sort.each do |trooper|
+  this_trooper = Hash.new
+  this_trooper = trooper[1]
+  print "#{trooper[0]}  "
+  print " #{this_trooper['rank']} #{this_trooper['first_name']} #{this_trooper['last_name']}  "
+  puts "Morale #{this_trooper['morale']}"
+end
 
-unit_max = unit.count - 1
+#unit_max = unit.count - 1
+# puts unit.class
+#pp unit["43"]
 
 def show_trooper(trooper)
   this_trooper = Hash.new
@@ -24,26 +28,27 @@ def show_trooper(trooper)
   puts "Morale: #{this_trooper['morale']} "
 end
 
+#show_trooper(unit["43"])
 #(0..unit_max).each do |index|
 #  index = index.to_s
 #  print "#{index} "
 #  puts unit[index]['rank']
 #end
 
-first_squad = [ 1, 2, 3, 4]
+# first_squad = [ 1, 2, 3, 4]
 
 #unit.keys.each do |key|
 #  puts key
 #end
 
 #first_squad.each do |id|
-unit.each do |trooper|
-  show_trooper(trooper)
+#unit.each do |key, trooper|
+#  show_trooper(trooper)
   #id = id.to_s
   #puts "id is #{id}."
   #puts id.class
   #trooper = unit[id] 
 #  show_trooper(trooper)
-end
+#end
 
 
--- ideas/unit/show_unit.rb
+++ ideas/unit/show_unit.rb
@@ -1,15 +1,68 @@
 #!/usr/bin/env ruby
 
-$LOAD_PATH << File.expand_path("./lib", __FILE__)
-
 require 'json'
-require 'Trooper'
 
-unit_file = File.open('battle_results.txt')
-unit = JSON.parse(unit_file)
+character_file = File.read('blue_dragon_roster.json')
+troop_list = JSON.parse(character_file)
 
-this_trooper = Trooper(unit['0']).new
-this_trooper.trooper_text
+unit_file = File.read('blue_dragon_unit_toe.json')
+unit_toe = JSON.parse(unit_file)
 
+def show_trooper(trooper)
+  %w[ rank first_name last_name morale ].each do |item|
+    print "  #{trooper[item]} "
+  end
+  print "\n"
+end
 
+def get_trooper_morale(trooper)
+  return trooper['morale']
+end
+
+def team_as_text(designation, members, troop_list)
+  ordinals = %w[ HQ 1st 2nd 3rd 4th 5th 6th 7th 8th 9th ]
+  unit = Hash.new
+  unit['designation'] = designation
+  designation_array = designation.split('.')
+  unit['members'] = members
+  total_morale = 0
+  unit['members'].each do |member|
+    total_morale += get_trooper_morale(troop_list[member]).to_i
+  end
+  unit_morale = total_morale.to_f / unit['members'].count
+  unit['morale'] = unit_morale.round
+
+  platoon = ordinals[designation_array[1].to_i]
+  squad = ordinals[designation_array[2].to_i]
+  team = ordinals[designation_array[3].to_i]
+
+  if designation.end_with?('0.0.0')
+    title = "Company Commander  "
+  elsif designation.end_with?('0.1.0') || designation.end_with?('0.0')
+    title = "#{platoon} Platoon Leader"
+  elsif designation.end_with?('.0')
+    ordinal = ordinals[designation_array[2].to_i]
+    title = "#{platoon} Platoon #{squad} Squad Leader"
+  else
+    title = "#{platoon} Platoon #{squad} Squad #{team} Team"
+  end
+  puts "#{title}  Morale:  #{unit['morale']}"
+  unit['members'].each do |member|
+    show_trooper(troop_list[member])
+  end  
+  puts
+end
+
+%w[name type size].each do |header|
+  print "#{unit_toe[header]} "
+end
+print "\n\n"
+
+unit_toe['teams'].sort.each do |key, value|
+  team_as_text(key, value, troop_list)
+end
+
+outfile = File.open('blue_dragon_unit_toe.json', 'w')
+outfile.write(JSON.pretty_generate(unit_toe))
+outfile.close
 
