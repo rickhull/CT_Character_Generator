736d6576b7022a69ce3e21ad3406634441d374f3
Leam Hall <leamhall@gmail.com>
2015-07-31 18:31:12 -0400

    Playing with OptionParser in show_unit.rb.

--- ideas/unit/show_unit.rb
+++ ideas/unit/show_unit.rb
@@ -1,36 +1,51 @@
 #!/usr/bin/env ruby
 
+$LOAD_PATH << File.expand_path('../lib', __FILE__)
+
 require 'json'
+require 'Trooper'
+require 'optparse'
 
-character_file = File.read('blue_dragon_roster.json')
-troop_list = JSON.parse(character_file)
+#roster_file = ''
+toe_file = ''
 
-unit_file = File.read('blue_dragon_unit_toe.json')
-unit_toe = JSON.parse(unit_file)
+troop_list = Hash.new
 
-def show_trooper(trooper)
-  %w[ rank first_name last_name morale ].each do |item|
-    print "  #{trooper[item]} "
+parser = OptionParser.new do |opts|
+  program_name = File.basename($PROGRAM_NAME)
+  opts.banner = "Used to display a unit TO&E.
+  Usage: #{program_name} -r <roster> -t <toe>"
+  opts.on( '-r <roster file>', 'Which team member unit roster file?') do |roster|
+    roster_file = roster
+    character_file = File.read(roster)
+    troop_list = JSON.parse(character_file)
   end
-  print "\n"
+  opts.on('-t <to&e file>', 'Which organization table?') do |toe|
+    toe_file = toe
+  end
 end
+parser.parse!
 
-def get_trooper_morale(trooper)
-  return trooper['morale']
-end
+#character_file = File.read(roster_file)
+#troop_list = JSON.parse(character_file)
+
+unit_file = File.read(toe_file)
+unit_toe = JSON.parse(unit_file)
 
 def team_as_text(designation, members, troop_list)
   ordinals = %w[ HQ 1st 2nd 3rd 4th 5th 6th 7th 8th 9th ]
   unit = Hash.new
   unit['designation'] = designation
   designation_array = designation.split('.')
-  unit['members'] = members
+
   total_morale = 0
-  unit['members'].each do |member|
-    total_morale += get_trooper_morale(troop_list[member]).to_i
+  unit_members = Array.new
+  members.each do |member|
+    unit_member = Trooper.new(troop_list[member])
+    total_morale += unit_member.trooper_morale?.to_i
+    unit_members << unit_member.trooper_text
   end
-  unit_morale = total_morale.to_f / unit['members'].count
-  unit['morale'] = unit_morale.round
+  unit_morale = total_morale.fdiv(members.count).round
 
   platoon = ordinals[designation_array[1].to_i]
   squad = ordinals[designation_array[2].to_i]
@@ -46,9 +61,9 @@ def team_as_text(designation, members, troop_list)
   else
     title = "#{platoon} Platoon #{squad} Squad #{team} Team"
   end
-  puts "#{title}  Morale:  #{unit['morale']}"
-  unit['members'].each do |member|
-    show_trooper(troop_list[member])
+  puts "#{title}  Morale:  #{unit_morale}"
+  unit_members.each do |member|
+    puts member
   end  
   puts
 end
@@ -62,7 +77,13 @@ unit_toe['teams'].sort.each do |key, value|
   team_as_text(key, value, troop_list)
 end
 
-outfile = File.open('blue_dragon_unit_toe.json', 'w')
-outfile.write(JSON.pretty_generate(unit_toe))
-outfile.close
+#<jhass> leitz: great. Note that we tend to leave get_ prefixes off in ruby and use foo? instead of is_foo
+# Done.
 
+#<jhass> leitz: to sum you can use .inject, unit["members"].inject(0) {|total, member| total + trooper_morale(member) }
+# Not sure this is still feasible as I put the Trooper into it's own class.
+
+#<jhass> a.fdiv(b) makes a nice replacement for a.to_f / b
+# Done.
+
+#<jhass> leitz: and you should use File.write/File.read or at least the block form of File.open, if you have a file.close somewhere you likely have a potential file handle leak ;)
