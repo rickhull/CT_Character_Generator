5266fc82707965038b991f5f23887f2235153819
Leam Hall <leamhall@gmail.com>
2015-09-12 11:49:51 -0400

    Traveller.noble? method, and tests, added.

--- lib/Traveller.rb
+++ lib/Traveller.rb
@@ -11,6 +11,29 @@ module Traveller
     end
     return @total.to_i
   end
+
+  def Traveller.noble?(gender, upp)
+    nobility = Hash.new
+    nobility['B'] = { 'f' => 'Dame',      'm' => 'Knight' }
+    nobility['C'] = { 'f' => 'Baroness',  'm' => 'Baron' }
+    nobility['D'] = { 'f' => 'Marquesa',  'm' => 'Marquis' }
+    nobility['E'] = { 'f' => 'Countess',  'm' => 'Count' }
+    nobility['F'] = { 'f' => 'Duchess',   'm' => 'Duke' }
+ 
+    title = '' 
+    soc = upp[5,1].upcase 
+    if nobility.has_key?(soc)
+      if gender.downcase == 'female'
+        title = nobility[soc]['f']
+      else
+        title = nobility[soc]['m']
+      end 
+    end
+    return title
+  end 
+
+
+
 end
 
     
--- spec/Traveller_spec.rb
+++ spec/Traveller_spec.rb
@@ -36,4 +36,92 @@ module Traveller
     end
   end
 
-end
+
+  describe 'Should return a title if nobility.' do
+    
+    it 'returns Knight if gender male and Soc B.' do
+      @gender = 'male'
+      @upp = '77777B'
+      @title = Traveller.noble?(@gender, @upp)
+      expect(@title).to eq('Knight')
+    end
+
+    it 'returns Dame if gender female and Soc B.' do
+      @gender = 'female'
+      @upp = '77777B'
+      @title = Traveller.noble?(@gender, @upp)
+      expect(@title).to eq('Dame')
+    end
+
+    it 'returns Baron if gender male and Soc C.' do
+      @gender = 'male'
+      @upp = '77777C'
+      @title = Traveller.noble?(@gender, @upp)
+      expect(@title).to eq('Baron')
+    end
+
+    it 'returns Baroness if gender female and Soc C.' do
+      @gender = 'female'
+      @upp = '77777C'
+      @title = Traveller.noble?(@gender, @upp)
+      expect(@title).to eq('Baroness')
+    end
+  
+    it 'returns Marquis if gender male and Soc D.' do
+      @gender = 'male'
+      @upp = '77777D'
+      @title = Traveller.noble?(@gender, @upp)
+      expect(@title).to eq('Marquis')
+    end
+
+    it 'returns Marquesa if gender female and Soc D.' do
+      @gender = 'female'
+      @upp = '77777D'
+      @title = Traveller.noble?(@gender, @upp)
+      expect(@title).to eq('Marquesa')
+    end
+    
+    it 'returns Count if gender male and Soc E.' do
+      @gender = 'male'
+      @upp = '77777E'
+      @title = Traveller.noble?(@gender, @upp)
+      expect(@title).to eq('Count')
+    end
+
+    it 'returns Countess if gender female and Soc E.' do
+      @gender = 'female'
+      @upp = '77777E'
+      @title = Traveller.noble?(@gender, @upp)
+      expect(@title).to eq('Countess')
+    end
+
+    it 'returns Duke if gender male and Soc F.' do
+      @gender = 'male'
+      @upp = '77777F'
+      @title = Traveller.noble?(@gender, @upp)
+      expect(@title).to eq('Duke')
+    end
+
+    it 'returns Duchess if gender female and Soc F.' do
+      @gender = 'female'
+      @upp = '77777F'
+      @title = Traveller.noble?(@gender, @upp)
+      expect(@title).to eq('Duchess')
+    end
+
+    it 'returns an empty string if gender male and Soc 2-A.' do
+      @gender = 'male'
+      @upp = '77777A'
+      @title = Traveller.noble?(@gender, @upp)
+      expect(@title).to eq('')
+    end
+
+    it 'returns an empty string if gender female and Soc 2-A.' do
+      @gender = 'female'
+      @upp = '77777A'
+      @title = Traveller.noble?(@gender, @upp)
+      expect(@title).to eq('')
+    end
+  end
+
+end 
