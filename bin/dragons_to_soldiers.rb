bdcc78b29dd30aa2171372b2e114324596f50bae
Leam Hall <leamhall@gmail.com>
2015-10-03 17:10:08 -0400

    Working on case for decorations.

--- bin/dragons_to_soldiers.rb
+++ bin/dragons_to_soldiers.rb
@@ -9,7 +9,7 @@ require 'Traveller'
 
 
 def write_soldiers(data)
-  soldier_file = File.open("#{DATA_DIR}/soldier.json", 'w')
+  soldier_file = File.open("#{DATA_DIR}/soldiers.json", 'w')
   soldier_file.write(JSON.pretty_generate(data))
   soldier_file.close
 end
@@ -31,22 +31,38 @@ def skills_to_hash(soldier)
     skill_name, skill_value = skill.split('-')
     soldier['skills'][skill_name] = skill_value.to_i
   end
+
+  if soldier['skills'].has_key?('Tactics')
+    soldier['morale_modifier'] += soldier['skills']['Tactics']
+  end 
+end
+
+def terms(soldier)
+  unless soldier.has_key?('terms')
+    age = soldier['age'].to_i
+    terms = (age - 18) / 4
+    terms = [terms, 0].max
+    puts "terms is #{terms}."
+    soldier['terms'] = terms
+  end
 end
 
 def add_awards(soldier)
+
   unless soldier.has_key?('awards')
     soldier['awards'] = Hash.new
   end
 
   award_list = %w[ CC CSR MCUF PH MCG SEH ]
-  leader_rank_list = %w[ LCP CPL SGT NCO LSG LT CPT ]
 
   if soldier['awards'].has_key?('CSR')
     soldier['awards']['CSR'] += 1
   else
     soldier['awards']['CSR'] = 1
   end
+  soldier['morale_modifier'] += 1
 
+  leader_rank_list = %w[ LCP CPL SGT NCO LSG LT CPT ]
   if leader_rank_list.include?(soldier['rank'])
     if soldier['awards'].has_key?('CC')
       soldier['awards']['CC'] += 1
@@ -54,17 +70,54 @@ def add_awards(soldier)
       soldier['awards']['CC'] = 1
     end
   end
+ 
+  wound_list = %w[ Light Serious Deadly ]
+  if wound_list.include?(soldier['wound'])
+    if soldier['awards'].has_key?('PH')
+      soldier['awards']['PH'] += 1
+    else
+      soldier['awards']['PH'] = 1
+    end
+    soldier['morale_modifier'] += 1
+  end
+
+  # The Marine Raid Decoration roll is 5+
+  decoration = ''
+  decoration_roll = Traveller.roll_dice(6,2) - 4
+  case decoration_roll
+    when 1..3: 
+      decoration = 'MCUF'
+      soldier['morale_modifier'] += 1
+    when 4..5: 
+      decoration = 'MCG'
+      soldier['morale_modifier'] += 2
+    when > 5:
+      decoration = 'SEH'
+      soldier['morale_modifier'] += 3
+  end
+  puts "decoration_roll is #{decoration_roll}, for #{decoration}."
   
+  unless decoration.length == 0
+    if soldier['awards'].has_key?(decoration)
+      soldier['awards'][decoration] += 1
+    else
+      soldier['awards'][decoration] = 1
+    end
+  end 
+
 end
 
 if valid_json
   soldiers = Hash[dragons]
-  puts "Found #{dragons.count} Dragons."
-  puts "Looking at Dragon 1."  
-  # puts dragons['1'].inspect
+  #puts "Found #{dragons.count} Dragons."
+  #puts "Looking at Dragon 1."  
+  #puts dragons['1'].inspect
  
   soldiers.each do |key, soldier|
+    soldier['morale_modifier'] = 0
+    terms(soldier)
     skills_to_hash(soldier)
+    add_awards(soldier)
   end
 
   
@@ -78,10 +131,12 @@ if valid_json
   soldiers['1']['awards'].each do |key, value|
     puts "#{key}: #{value}"
   end
+  puts "Morale Modifier: #{soldiers['1']['morale_modifier']}."
+  puts "Terms: #{soldiers['1']['terms']}."
  
-
+  write_soldiers(soldiers)
 else
-  puts "Kinds lost."
+  puts "Kinda lost."
 end
 
 
