0bdc2102f10e8fd9a841a0d7de24a31db2c7c262
Leam Hall <leamhall@gmail.com>
2015-11-15 19:09:53 -0500

    Added test_module.rb for feedback.

---
+++ ideas/test_module.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+module Marine
+  def show_rank
+    puts "I am a Marine!"
+  end
+end
+
+module Pirate
+  def show_rank
+    puts "Arrgghhh!!"
+  end
+end
+
+class Character
+  include Marine
+  include Pirate
+
+  def set_career(career)
+    @career = career
+    career.show_rank
+  end
+end
+
+Fred = Character.new
+Fred.set_career('Marine')
+Fred.show_rank
+
+
+
