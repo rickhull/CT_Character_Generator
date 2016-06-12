248ae1511a517f18d0d60b7ddaac3de760bd15b9
Leam Hall <leamhall@gmail.com>
2016-06-12 17:54:26 -0400

    Added age, skills, and careers to Character.rb.

--- lib/Character.rb
+++ lib/Character.rb
@@ -5,14 +5,17 @@ class Character
   $DATA_PATH = File.expand_path("../../data", __FILE__)
 
   require 'Traveller'
-  attr_accessor :gender, :name, :upp
-                # :career, :officer, :morale, :rank, :terms,
-                # :awards, :wounds, :skills, :age, :title
+  attr_accessor :gender, :name, :upp, :skills, :careers, :age
+                # :officer, :morale, :rank, :terms,
+                # :awards, :wounds, :title
 
   def initialize()
     @upp      = Traveller.upp
     @gender   = Traveller.gender.capitalize
     @name     = Traveller.name(@gender)
+    @skills   = Hash.new
+    @careers  = Hash.new
+    @age      = 18
   end
   
 end
