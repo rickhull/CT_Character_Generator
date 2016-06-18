2ff62ee244e26fdb3c25849448024eb7133d4209
Leam Hall <leamhall@gmail.com>
2016-06-18 09:20:48 -0400

    Minimized Character.rb.

--- lib/Character.rb
+++ lib/Character.rb
@@ -1,12 +1,6 @@
 class Character
 
-
-  $LOAD_PATH << File.expand_path("../../lib", __FILE__)
-  $DATA_PATH = File.expand_path("../../data", __FILE__)
-
   attr_accessor :gender, :name, :upp, :skills, :careers, :age
-                # :officer, :morale, :rank, :terms,
-                # :awards, :wounds, :title
 
   def initialize()
     @upp      = String.new
@@ -14,7 +8,6 @@ class Character
     @name     = String.new
     @skills   = Hash.new
     @careers  = Hash.new
-    @age      = 18
   end
-  
+ 
 end
