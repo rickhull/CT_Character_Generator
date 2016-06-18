0ca2a17bffcaef32711e526cdf5d7a0dfc968b18
Leam Hall <leamhall@gmail.com>
2016-06-18 08:31:54 -0400

    Putting initial data creation into CharacterCreate.rb.

--- lib/Character.rb
+++ lib/Character.rb
@@ -4,15 +4,14 @@ class Character
   $LOAD_PATH << File.expand_path("../../lib", __FILE__)
   $DATA_PATH = File.expand_path("../../data", __FILE__)
 
-  require 'Traveller'
   attr_accessor :gender, :name, :upp, :skills, :careers, :age
                 # :officer, :morale, :rank, :terms,
                 # :awards, :wounds, :title
 
   def initialize()
-    @upp      = Traveller.upp
-    @gender   = Traveller.gender.capitalize
-    @name     = Traveller.name(@gender)
+    @upp      = String.new
+    @gender   = String.new
+    @name     = String.new
     @skills   = Hash.new
     @careers  = Hash.new
     @age      = 18
---
+++ lib/CharacterCreate.rb
@@ -0,0 +1,18 @@
+module CharacterCreate
+
+  $LOAD_PATH << File.expand_path("../../lib", __FILE__)
+  $DATA_PATH = File.expand_path("../../data", __FILE__)
+
+  require 'Traveller'
+  require 'Character'
+
+  def self.init()
+    character          = Character.new
+    character.upp      = Traveller.upp
+    character.gender   = Traveller.gender.capitalize
+    character.name     = Traveller.name(character.gender)
+    character.age      = 18
+    return character
+  end
+  
+end
