c3d3fc3d0772973a03922d206c16ec7e69d3a951
Leam Hall <leamhall@gmail.com>
2016-06-18 11:52:43 -0400

    Removed old code from test_CharacterTools.rb.

--- lib/test_CharacterTools.rb
+++ lib/test_CharacterTools.rb
@@ -5,13 +5,6 @@ CharacterTools.add_career(character, 'Navy', 2)
 CharacterTools.add_career(character, 'Navy', 2)
 CharacterTools.add_career(character, 'Marines', 2)
 
-#puts "#{character.name}  #{character.gender} Age: #{character.age} #{character.upp}"
-#character.careers.each do |k,v|
-#  printf("%s %s terms  ", k, v)
-#end
-#puts
-
 data_file = '../data/new_characters.json'
 CharacterTools.save_character(character, data_file, 'json')
-#CharacterTools.show_character_file(data_file, 'json')
 CharacterTools.show_characters(data_file, 'json')
