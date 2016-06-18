38a8efb025257153821fd62166ce72e14088ce10
Leam Hall <leamhall@gmail.com>
2016-06-18 08:34:54 -0400

    The test_ file for making sure code works.

---
+++ lib/test_CharacterCreate.rb
@@ -0,0 +1,11 @@
+
+require 'CharacterCreate'
+
+character = CharacterCreate.init
+name      = character.name
+upp       = character.upp
+age       = character.age
+gender    = character.gender
+
+puts "#{name}  #{gender} Age: #{age}"
+puts "#{upp}"
