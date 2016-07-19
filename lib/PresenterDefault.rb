f98e00d8f4ed9ef73b1090a21da93daa363420dc
Leam Hall <leamhall@gmail.com>
2016-07-19 10:00:35 -0400

    Initial PresenterDefault. It shows characters in simple format.

---
+++ lib/PresenterDefault.rb
@@ -0,0 +1,17 @@
+module PresenterDefault
+
+
+  def PresenterDefault.show(character)
+    print "#{character.name} "
+    print "#{character.upp} "
+    print "#{character.age} "
+    character.careers.each_pair do |career, terms|
+      print " #{career}: #{terms} "
+    end
+    if character.skills 
+      character.skills.each_pair do |skill, level|
+        print "#{skill}-#{level} "
+      end
+    end
+  end
+end
