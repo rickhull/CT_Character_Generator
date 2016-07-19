fc8a1a2dc0f5cf629a51df23ae1817fdf8c365b3
Leam Hall <leamhall@gmail.com>
2016-07-19 09:45:03 -0400

    Added randomization for career and terms to characterTools.rb.

--- lib/test_CharacterTools.rb
+++ lib/test_CharacterTools.rb
@@ -1,12 +1,11 @@
 require 'CharacterTools'
 
 character = CharacterTools.init
-#CharacterTools.add_career(character, 'Navy', 2)
-#CharacterTools.add_career(character, 'Navy', 2)
-#CharacterTools.add_career(character, 'Marine', 2)
-
-CharacterTools.add_career(character, 'Marine', 1)
-
+careers = ['Navy', 'Scout', 'Merchant', 'Army', 'Marine', 'Other']
+career_choice = rand(careers.length)
+career = careers[career_choice]
+terms = rand(5) + 1
+CharacterTools.add_career(character, career, terms)
 
 data_file = '../data/new_characters.json'
 CharacterTools.save_character(character, data_file, 'json')
