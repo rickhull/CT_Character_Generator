29ce10d12ff87867b9263bfa900dfd0378ac2643
Leam Hall <leamhall@gmail.com>
2016-07-20 06:57:00 -0400

    Added test/ts_CT_Character_Generator.
    Moved the existing "test_<file>" programs to "tc_<file>" for naming conventions.
    
    All tests currently pass.

---
+++ test/ts_CT_Character_Generator.rb
@@ -0,0 +1,9 @@
+# This is the cumulative test suite for the entire
+# CT_Character_Generator program. Test Cases will be 
+# written for each component program and named
+# "tc_<Program>.rb
+
+require 'test/unit'
+require 'tc_CharacterTools'
+require 'tc_Traveller'
+
