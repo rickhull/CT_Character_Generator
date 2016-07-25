44b7584d67ea55e0059da25bb13dcf93b181d3fd
Leam Hall <leamhall@gmail.com>
2016-07-25 07:33:20 -0400

    Changed show_Character.rb to use CharacterTools vice Character.

--- bin/show_Character.rb
+++ bin/show_Character.rb
@@ -1,17 +1,19 @@
-require 'Character'
+$LOAD_PATH << File.expand_path("../../lib", __FILE__)
+
+require 'CharacterTools'
 require 'Traveller'
 
 10.times do
-    person = Character.new
+    person = CharacterTools.init
     upp = person.upp
     gender = person.gender
+    name = person.name
+    puts "Hi, I'm #{name}, a #{gender} with a upp of #{upp}."
     skills = person.skills
     skills = Traveller.add_skill(skills, 'GunCbt', 1)
     person.skills = skills
     person.skills.each do |k,v|
       puts "#{k}-#{v}"
     end
-    name = person.name
-    puts "Hi, I'm #{name}, a #{gender} with a upp of #{upp}."
 end
 
