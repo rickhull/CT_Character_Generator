dc5f6bebfde41588ac1ce88c29947531fee7fcd2
Leam Hall <leamhall@gmail.com>
2016-07-27 07:38:38 -0400

    Setting a specific character in test_Presenter.rb.

--- test/test_Presenter.rb
+++ test/test_Presenter.rb
@@ -1,7 +1,13 @@
+$LOAD_PATH << File.expand_path("../../lib", __FILE__)
+
 require 'Presenter'
 require 'CharacterTools'
 
 character = CharacterTools.init
-
+character.name      = "Marco Domici"
+character.upp       = "79AA8B" 
+character.skills    = {'GunCbt' => 1, 'VaccSuit' => 1}
+character.careers   = {'Marine' => 2, 'Noble' => 1}
+Presenter.show(character)
 Presenter.show(character, 'csv')
-
+#Presenter.show(character, 'wiki')
