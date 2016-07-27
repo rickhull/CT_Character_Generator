e24404d5b7c33561b8b98b5d2d12dd2e4835ecad
Leam Hall <leamhall@gmail.com>
2016-07-27 09:34:19 -0400

    Started changing Marine.rb for the new model.

--- lib/Marine.rb
+++ lib/Marine.rb
@@ -1,36 +1,55 @@
 module Marine
-  require 'Traveller'
 
-  #self.Grade = Hash.new
-  #self.Grade['Enlisted'] = %w(E1 E2 E3 E4 E5 E6 E7 E8 E9)
-  #self.Grade['Officer'] = %w(O1 O2 O3 O4 O5 O6)
-#
-  #self.Ranks = { 
-  #  'E1' => 'Private',
-  #  'E2' => 'Lance Corporal',
-  #  'E3' => 'Corporal',
-  #  'E4' => 'Lance Sergeant',
-  #  'E5' => 'Sergeant',
-  #  'E6' => 'Leading Sergeant',
-  #  'E7' => 'First Sergeant',
-  #  'E8' => 'Gunnery Sergeant',
-  #  'E9' => 'Sergeant Major',
-  #  'O1' => 'Lieutenant',
-  #  'O2' => 'Captain',
-  #  'O3' => 'Force Commander',
-  #  'O4' => 'Lieutenant Colonel',
-  #  'O5' => 'Colonel',
-  #  'O6' => 'Brigadier'
-  #}
-#
-    #@careers << 'Marine'
-    #@comission_roll = 9
-    #@grade_set = 'Enlisted'
-    #@rank = 'Private'
-    #@officer = officer(@comission_roll)
-    @skill_options = [ 'Brawling', 'Gambling', '+1 Str', '+1 Dex', '+1 End', 'Blade', 'Vehicle', 'VaccSuit', 'Blade', 'GunCbt', 'GunCbt', 'Vehicle', 'Mechanical', 'Electronic', 'Tactics', 'Blade', 'GunCbt']
-    @advanced_skill_options = ['Medical', 'Tactics', 'Tactics', 'Computer', 'Admin', 'Leader']
-  end
+  self.grade = "E1"
+
+  self.Ranks = { 
+    'E1' => 'Private',
+    'E2' => 'Lance Corporal',
+    'E3' => 'Corporal',
+    'E4' => 'Lance Sergeant',
+    'E5' => 'Sergeant',
+    'E6' => 'Leading Sergeant',
+    'E7' => 'First Sergeant',
+    'E8' => 'Gunnery Sergeant',
+    'E9' => 'Sergeant Major',
+    'O1' => 'Lieutenant',
+    'O2' => 'Captain',
+    'O3' => 'Force Commander',
+    'O4' => 'Lieutenant Colonel',
+    'O5' => 'Colonel',
+    'O6' => 'Brigadier'
+  }
+
+    #careers << 'Marine'
+    comission_roll = 9
+    skill_options = [ 
+      'Brawling', 
+      'Gambling', 
+      '+1 Str', 
+      '+1 Dex', 
+      '+1 End', 
+      'Blade', 
+      'Vehicle', 
+      'VaccSuit', 
+      'Blade', 
+      'GunCbt', 
+      'GunCbt', 
+      'Vehicle', 
+      'Mechanical', 
+      'Electronic', 
+      'Tactics', 
+      'Blade', 
+      'GunCbt'
+      ]
+    
+    advanced_skill_options = [
+      'Medical', 
+      'Tactics', 
+      'Tactics',  
+      'Computer', 
+      'Admin', 
+      'Leader'
+      ]
 
   def Marine.first_term(character)
     Traveller.add_skill(character.skills, 'Blade', 1)
