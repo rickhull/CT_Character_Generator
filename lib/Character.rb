6df0bdd7c01516ee83e466abd5305b3fd876f163
Leam Hall <leamhall@gmail.com>
2016-07-30 12:07:56 -0400

    Added mustering out benefits to Character.rb.

--- lib/Character.rb
+++ lib/Character.rb
@@ -1,6 +1,7 @@
 class Character
 
-  attr_accessor :gender, :name, :upp, :skills, :careers, :age, :rank, :title
+  attr_accessor :gender, :name, :upp, :skills, 
+      :careers, :age, :rank, :title, :stuff
 
   def initialize()
     @upp      = String.new
@@ -8,6 +9,9 @@ class Character
     @name     = String.new
     @skills   = Hash.new(0)
     @careers  = Hash.new(0)
+    @stuff    = { 'cash' => 0,
+      'benefits'  => Hash.new(0)
+      }
   end
  
 end
