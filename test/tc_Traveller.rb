3f5d0bd7b3b814517e599e12d2fedecfa44bc315
Leam Hall <leamhall@gmail.com>
2016-07-30 12:07:21 -0400

    Removing character stuff tools from tc_Traveller.rb.

--- test/tc_Traveller.rb
+++ test/tc_Traveller.rb
@@ -29,77 +29,77 @@ class TestTraveller < Test::Unit::TestCase
     end
   end
 
-  def test_gender
-    genders = ['female', 'male']
-    200.times do |count|
-      assert(genders.include?(Traveller.gender))
-    end
-  end
+  #def test_gender
+  #  genders = ['female', 'male']
+  #  200.times do |count|
+  #    assert(genders.include?(Traveller.gender))
+  #  end
+  #end
 
-  def test_modify_stat
-    upp = "777777"
-    assert(Traveller.modify_stat(upp,0,+8) == "F77777")
-    upp = "777777"
-    assert(Traveller.modify_stat(upp,0,+9) == "F77777")
-    upp = "777777"
-    assert(Traveller.modify_stat(upp,0,-9) == "077777")
-  end
+  #def test_modify_stat
+  #  upp = "777777"
+  #  assert(Traveller.modify_stat(upp,0,+8) == "F77777")
+  #  upp = "777777"
+  #  assert(Traveller.modify_stat(upp,0,+9) == "F77777")
+  #  upp = "777777"
+  #  assert(Traveller.modify_stat(upp,0,-9) == "077777")
+  #end
 
-  def test_set_stat
-    upp = "777777"
-    assert(Traveller.set_stat(upp,0,"F") == "F77777")
-  end
+  #def test_set_stat
+  #  upp = "777777"
+  #  assert(Traveller.set_stat(upp,0,"F") == "F77777")
+  #end
 
-  def test_noble?
-    upp = "77777F"
-    assert(Traveller.noble?(upp))
-  end
+  #def test_noble?
+  #  upp = "77777F"
+  #  assert(Traveller.noble?(upp))
+  #end
 
-  def test_noble
-    # Should be nil, non-noble values.
-    upp = "77777A"
-    gender = "male"
-    assert(Traveller.noble(gender,upp) == "")
-    upp = "77777G"
-    gender = "female"
-    assert(Traveller.noble(gender,upp) == "")
+  #def test_noble
+  #  # Should be nil, non-noble values.
+  #  upp = "77777A"
+  #  gender = "male"
+  #  assert(Traveller.noble(gender,upp) == "")
+  #  upp = "77777G"
+  #  gender = "female"
+  #  assert(Traveller.noble(gender,upp) == "")
     
-    #Should be correct.
-    gender = "female"
-    upp = "77777B"
-    assert(Traveller.noble(gender, upp) == "Dame")
-    upp = "77777C" 
-    assert(Traveller.noble(gender, upp) == "Baroness")
-    upp = "77777D" 
-    assert(Traveller.noble(gender, upp) == "Marquesa")
-    upp = "77777E" 
-    assert(Traveller.noble(gender, upp) == "Countess")
-    upp = "77777F" 
-    assert(Traveller.noble(gender, upp) == "Duchess")
-    gender = "male"
-    upp = "77777B"
-    assert(Traveller.noble(gender, upp) == "Knight")
-    upp = "77777C" 
-    assert(Traveller.noble(gender, upp) == "Baron")
-    upp = "77777D" 
-    assert(Traveller.noble(gender, upp) == "Marquis")
-    upp = "77777E" 
-    assert(Traveller.noble(gender, upp) == "Count")
-    upp = "77777F" 
-    assert(Traveller.noble(gender, upp) == "Duke")
-  end
+  #  #Should be correct.
+  #  gender = "female"
+  #  upp = "77777B"
+  #  assert(Traveller.noble(gender, upp) == "Dame")
+  #  upp = "77777C" 
+  #  assert(Traveller.noble(gender, upp) == "Baroness")
+  #  upp = "77777D" 
+  #  assert(Traveller.noble(gender, upp) == "Marquesa")
+  #  upp = "77777E" 
+  #  assert(Traveller.noble(gender, upp) == "Countess")
+  #  upp = "77777F" 
+  #  assert(Traveller.noble(gender, upp) == "Duchess")
+  #  gender = "male"
+  #  upp = "77777B"
+  #  assert(Traveller.noble(gender, upp) == "Knight")
+  #  upp = "77777C" 
+  #  assert(Traveller.noble(gender, upp) == "Baron")
+  #  upp = "77777D" 
+  #  assert(Traveller.noble(gender, upp) == "Marquis")
+  #  upp = "77777E" 
+  #  assert(Traveller.noble(gender, upp) == "Count")
+  #  upp = "77777F" 
+  #  assert(Traveller.noble(gender, upp) == "Duke")
+  #end
 
-  def test_add_skill
-    skills = Hash.new(0)
-    skills["GunCbt"] = 2
-    Traveller.add_skill(skills, 'GunCbt', 1)
-    assert(skills['GunCbt'] == 3) 
-    new_skills = {"GunCbt" => 3, "Pilot" => 1}
-    Traveller.add_skill(skills, 'Pilot', 1)
-    assert(skills == new_skills)
-    very_new_skills = {"GunCbt" => 3, "Pilot" => 1, "Leader" => 1}
-    Traveller.add_skill(skills, 'Leader')
-    assert(skills == very_new_skills)
-  end
+  #def test_add_skill
+  #  skills = Hash.new(0)
+  #  skills["GunCbt"] = 2
+  #  Traveller.add_skill(skills, 'GunCbt', 1)
+  #  assert(skills['GunCbt'] == 3) 
+  #  new_skills = {"GunCbt" => 3, "Pilot" => 1}
+  #  Traveller.add_skill(skills, 'Pilot', 1)
+  #  assert(skills == new_skills)
+  #  very_new_skills = {"GunCbt" => 3, "Pilot" => 1, "Leader" => 1}
+  #  Traveller.add_skill(skills, 'Leader')
+  #  assert(skills == very_new_skills)
+  #end
 
 end
