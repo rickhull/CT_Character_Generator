143b0b8f6e5e766a1e55bd30d4b1b063c7a649a2
Leam Hall <leamhall@gmail.com>
2016-08-13 09:26:03 -0400

    Added CareerTemplate.rb, with comments.

---
+++ lib/CareerTemplate.rb
@@ -0,0 +1,77 @@
+# Template for new Careers. To create your own Career:
+
+# 1. Change Mycareer to your career name. One word, and Ruby 
+#     classes must begin with an uppercase Alpha character.
+#
+# 2. Change the skill_options array to use the list of skills
+#     that don't require Edu 8+. Look at existing skill lists
+#     for the format. 
+#    You can duplicate entries if a career should have a higher
+#     instance of some skill. 
+#
+# 3. Change advanced_skill_options for skills requiring an Edu 8+
+#     to get in CharGen.
+#
+# 4. Change values in muster_out['cash'] array.
+#
+# 5. Change values in muster_out['benefits'] array.
+#
+# 6. Change the rank method if the career has ranks. 
+#     Add a commission roll as needed.
+#     Add the names of the ranks. 
+#     Recommended, but not required, to use terms as roll modifier.
+#
+
+$LOAD_PATH << File.expand_path("../../lib", __FILE__)
+
+require 'CharacterTools'
+require 'Career'
+require 'Traveller'
+
+class Mycareer < Career 
+  def initialize(char)
+    @skill_options = [ 
+      '+1 Str',
+      '+1 Dex',
+      '+1 End', 
+      '+1 Int', 
+      'Carouse', 
+      'Brawling', 
+      'GunCbt', 
+      'Blade', 
+      'Hunting', 
+      'Drive(any)',
+      'Bribery', 
+      '+1 Dex',
+      'Pilot', 
+      'ShipsBoat', 
+      'Drive(any)', 
+      'Navigation', 
+      'Engineering', 
+      'Leader'
+      ]
+    @advanced_skill_options = [
+      'Medic',
+      'Computer',
+      'Admin',
+      'Liaison',
+      'Leader',
+      'JoT'
+      ]
+
+    @muster_out = Hash.new
+    @muster_out['cash'] = [10000, 50000, 50000, 100000, 100000, 100000, 200000]
+    @muster_out['benefits'] = [
+      'HighPsg',
+      'HighPsg',
+      'Gun',
+      'Blade',
+      'TAS',
+      'Yacht'
+    ] 
+    super(char) 
+  end
+
+  def rank(char)
+  end
+end
