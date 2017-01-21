e942fd3d9fb6fe708c7cb4b1cd0a84500fd9dce3
Leam Hall <leamhall@gmail.com>
2017-01-21 06:24:00 -0500

    Updated LEO.rb with skill recommendations from CoTI.

--- lib/LEO.rb
+++ lib/LEO.rb
@@ -42,41 +42,55 @@ class LEO < Career
       "+1 Dex",
       "+1 End", 
       "+1 Int", 
-      "Carouse", 
-      "Brawling", 
-      "GunCbt", 
+      "Athletics",
       "Blade", 
-      "Vehicle",
+      "Brawling", 
       "Bribery", 
-      "Pilot", 
-      "ShipsBoat", 
-      "Engineering", 
-      "Leader"
+      "Carouse", 
+      "Deception",
+      "GunCbt", 
+      "Intrusion",
+      "Leader",
+      "Persuade",
+      "Recon",
+      "Stealth",
+      "Streetwise",
+      "Vehicle"
       ]
     @advanced_skill_options = [
-      "Medic",
-      "Computer",
       "Admin",
+      "Computer",
+      "Electronics",
+      "Interrogation",
+      "Investigate",
+      "Legal",
       "Liaison",
-      "Leader",
-      "JoT"
+      "Science(Forensic)",
+      "Tactics"
       ]
 
     @muster_out = Hash.new
-    @muster_out["cash"] = [10000, 50000, 50000, 100000, 100000, 100000, 200000]
+    @muster_out["cash"] = [1000, 5000, 5000, 10000, 10000, 10000, 20000]
     @muster_out["benefits"] = [
-      "HighPsg",
-      "HighPsg",
+      "LowPsg",
+      "MedPsg",
       "Gun",
-      "Blade",
-      "TAS",
-      "Yacht"
     ] 
     super(char) 
   end
 
+  def first_term(char)
+    career_skill_list     = ["Admin", "Carouse", "Computer", "GunCbt", "Investigate", "Streetwise", "Science(Forensic)"]
+    career_skill          = career_skill_list[rand(career_skill_list.length - 1)]
+    options               = Hash.new(0)
+    options["character"]  = char["character"]
+    options["level"]      = 1
+    options["skill"]      = career_skill
+    CharacterTools.increase_skill(options)
+  end
+
   def rank(char)
     ranks = %w[Recruit Officer Patrol Sergeant Lieutenant Captain Chief]
-    char['character'].rank = ranks[char["terms"]]
+    char["character"].rank = ranks[char["terms"]]
   end
 end
