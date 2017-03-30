937b9ae4230fff10b10a4057d11720fa2de0361d
Leam Hall <leamhall@gmail.com>
2017-03-30 06:15:07 -0400

    Fixed, in one sense, the global  issue.

--- toys/menu_system.rb
+++ toys/menu_system.rb
@@ -1,38 +1,40 @@
 
 require 'json'
 
-def base(characters)
+def base
+  puts "#" * 60
   puts "1) Search by first name"
   puts "2) Search by last name"
   puts "3) Edit character"
   puts "4) Add character"
   puts "x) Exit"
+  puts "#" * 60
   input = gets.chomp
   case input
-    when "1"  then  search_first_name(characters)
+    when "1"  then  search_first_name
     when "2"  then  search_last_name
     when "3"  then  edit_character
     when "4"  then  add_character
     when "x"  then  exit
     else base
   end
+  puts "#" * 60
 end
 
-def search_first_name(characters)
+def search_first_name
   puts "Enter a first name string"
   search_term = gets.chomp
   puts "Searching for '#{search_term}'."
   matches = Array.new
-  matches = get_matching_ids(characters, "first_name", search_term)
+  matches = get_matching_ids("first_name", search_term)
   if matches.count > 0
     matches.each do |match|
-      puts match
-      #puts "#{match}) #{characters[match]["first_name"]} #{characters[match]["last_name"]}"
+      puts "#{match}) #{$characters[match]["first_name"]} #{$characters[match]["last_name"]}"
     end
   else
     puts "No results found."
   end
-  base(characters)
+  base
 end
 
 def search_last_name
@@ -43,11 +45,11 @@ def search_last_name
   base
 end
 
-def get_matching_ids(hash, key, search_term)
+def get_matching_ids(key, search_term)
   matches = Array.new
-  hash.keys.each do |id|
-    if hash[id][key].downcase.include?(search_term.downcase)
-      puts hash[id][key]
+  $characters.keys.each do |id|
+    if $characters[id][key].downcase.include?(search_term.downcase)
+      puts $characters[id][key]
       matches << id
     end
   end
@@ -81,6 +83,6 @@ end
 ## Main ##
 #first_names = Array.new
 #last_names = Array.new
-characters = Hash.new
-characters = data_to_json('../data/blue_dragon_roster.json')
-base(characters)
+$characters = Hash.new
+$characters = data_to_json('../data/blue_dragon_roster.json')
+base
