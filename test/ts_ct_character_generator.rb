5bf4fa5bdc3827b0fb9b4637d281f82d8109e2fc
Leam Hall <leamhall@gmail.com>
2017-11-22 18:00:25 -0500

    Getting tc_navy to work.

--- test/ts_ct_character_generator.rb
+++ test/ts_ct_character_generator.rb
@@ -3,25 +3,25 @@
 # written for each component program and named
 # "tc_<Program>.rb
 
-$LOAD_PATH << File.expand_path("../../lib/Careers", __FILE__)
-$LOAD_PATH << File.expand_path("../../lib/Tools", __FILE__)
-$LOAD_PATH << File.expand_path("../../test", __FILE__)
+#$LOAD_PATH << File.expand_path("../../lib/Careers", __FILE__)
+#$LOAD_PATH << File.expand_path("../../lib/Tools", __FILE__)
+#$LOAD_PATH << File.expand_path("../../test", __FILE__)
 
 require "test/unit"
 
 # The infrastructure
-require "tc_CharacterTools"
-require "tc_Dice"
-require "tc_Name"
-require "tc_Character"
+#require "test/tc_character_tools"
+#require "tc_dice"
+#require "tc_name"
+#require "tc_character"
 #require "tc_Chargen"
 #require "tc_Career"
 #require "tc_PresenterDefault"
 
 # The various careers
-require "tc_Noble"
-require "tc_Other"
-require "tc_Citizen"
+#require "tc_Noble"
+#require "tc_Other"
+#require "tc_Citizen"
 #require "tc_Firster"
 
 #require "tc_Army"
@@ -29,4 +29,4 @@ require "tc_Citizen"
 #require "tc_LEO"
 #require "tc_Marine"
 #require "tc_Merchant"
-require "tc_Navy"
+require "tc_navy"
