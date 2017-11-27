ddf7917aaa83e499e5c4cf25994221e86041a7f6
Rick Hull <rick.hull@gmail.com>
2017-11-27 20:38:02 +0000

    add Stats and Description

--- lib/traveller/character.rb
+++ lib/traveller/character.rb
@@ -1,23 +1,45 @@
-module Traveller
-  class Character
-    def initialize(basic: {}, upp: {}, skills: {}, careers: {}, stuff: {})
-      @name = basic.fetch(:name)
-      @gender = basic.fetch(:gender)
-      @appearance = basic.fetch(:appearance)
-      @age = basic.fetch(:age)
-      @plot = basic.fetch(:plot)
-      @temperament = basic.fetch(:temperament)
+require 'traveller'
 
-      @strength = upp.fetch(:strength)
-      @dexterity = upp.fetch(:dexterity)
-      @endurance = upp.fetch(:endurance)
-      @intelligence = upp.fetch(:intelligence)
-      @education = upp.fetch(:education)
-      @social_status = upp.fetch(:social_status)
-
-      @skills = skills
-      @careers = careers
-      @stuff = stuff
+class Traveller::Character
+  Stats = Struct.new(:strength, :dexterity, :endurance,
+                     :intelligence, :education, :social_status) do
+    def self.roll(spec = '2d6')
+      self.new(*Array.new(6) { Traveller.roll spec })
     end
+
+    def self.empty
+      self.new(*Array.new(6) { 0 })
+    end
+
+    def boost(hsh)
+      hsh.each { |k,v| self[k] += v if self[k] }
+      self
+    end
+  end
+
+  Description = Struct.new(:name, :gender, :age,
+                           :appearance, :plot, :temperament) do
+    def self.new_with_hash(hsh)
+      self.new(hsh[:name], hsh[:gender], hsh[:age],
+               hsh[:appearance], hsh[:plot], hsh[:temperament])
+    end
+
+    def merge(other)
+      other = self.class.new_with_hash(other) if other.is_a?(Hash)
+      self.class.new(other.name || self.name,
+                     other.gender || self.gender,
+                     other.age    || self.age,
+                     other.appearance || self.appearance,
+                     other.plot       || self.plot,
+                     other.temperament || self.temperament)
+    end
+  end
+
+  def initialize(desc:, stats:, skills: Hash.new(0), careers: {}, stuff: {})
+    @desc = desc
+    @stats = stats
+    @skills = skills
+    @careers = careers
+    @stuff = stuff
   end
 end
