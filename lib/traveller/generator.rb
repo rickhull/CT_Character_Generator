8991a251f75e37982ed8b6306c54cc1f132fe263
Rick Hull <rick.hull@gmail.com>
2017-11-27 20:40:01 +0000

    use updated character stuff

--- lib/traveller/generator.rb
+++ lib/traveller/generator.rb
@@ -1,66 +1,55 @@
-require 'traveller/data'
 require 'traveller/character'
+require 'traveller/data'
 
-module Traveller
-  class Character
-    def self.generate(basic = {})
-      self.new(basic: Generator.basic.merge(basic), upp: Generator.upp)
+module Traveller::Character::Generator
+  C = Traveller::Character
+  D = Traveller::Data
+
+  def self.character(descr = {})
+    C.new(desc: self.desc.merge(descr),
+          stats: C::Stats.roll)
+  end
+
+  def self.name(gender)
+    case gender.to_s.downcase
+    when 'm', 'male'
+      D.sample('male_names.txt')
+    when 'f', 'female'
+      D.sample('female_names.txt')
+    else
+      raise "unknown gender: #{gender}"
     end
   end
 
-  module Generator
-    def self.name(gender)
-      case gender.to_s.downcase
-      when 'm', 'male'
-        Traveller::Data.sample('male_names.txt')
-      when 'f', 'female'
-        Traveller::Data.sample('female_names.txt')
-      else
-        raise "unknown gender: #{gender}"
-      end
-    end
+  def self.gender
+    Traveller.roll(dice: 1) > 3 ? 'M' : 'F'
+  end
 
-    def self.gender
-      Traveller.roll(dice: 1) > 3 ? 'M' : 'F'
-    end
+  def self.hair(tone: nil, body: nil, color: nil, length: nil)
+    tone ||= D.sample('hair_tone.txt')
+    body ||= D.sample('hair_body.txt')
+    color ||= D.sample('hair_colors.txt')
+    length ||= D.sample('hair_length.txt')
+    [tone, body, color, length].join(' ')
+  end
 
-    def self.hair(tone: nil, body: nil, color: nil, length: nil)
-      tone ||= Traveller::Data.sample('hair_tone.txt')
-      body ||= Traveller::Data.sample('hair_body.txt')
-      color ||= Traveller::Data.sample('hair_colors.txt')
-      length ||= Traveller::Data.sample('hair_length.txt')
-      [tone, body, color, length].join(' ')
-    end
+  def self.appearance(hair: nil, skin: nil)
+    hair ||= self.hair
+    skin ||= D.sample('skin_tones.txt')
+    "#{hair} hair with #{skin} skin"
+  end
 
-    def self.appearance(hair: nil, skin: nil)
-      hair ||= self.hair
-      skin ||= Traveller::Data.sample('skin_tones.txt')
-      "#{hair} hair with #{skin} skin"
-    end
+  def self.plot
+    D.sample('plots.txt')
+  end
 
-    def self.plot
-      Traveller::Data.sample('plots.txt')
-    end
+  def self.temperament
+    D.sample('temperaments.txt')
+  end
 
-    def self.temperament
-      Traveller::Data.sample('temperaments.txt')
-    end
-
-    def self.basic
-      gender = self.gender
-      { name: self.name(gender),
-        gender: gender,
-        appearance: self.appearance,
-        age: 18,
-        plot: self.plot,
-        temperament: self.temperament }
-    end
-
-    def self.upp
-      [:strength, :dexterity, :endurance,
-       :intelligence, :education, :social_status].reduce({}) { |hsh, sym|
-        hsh.merge(sym => Traveller.roll(faces: 6, dice: 2, count: 1))
-      }
-    end
+  def self.desc
+    gender = self.gender
+    C::Description.new(self.name(gender), gender, 18,
+                       self.appearance, self.plot, self.temperament)
   end
 end
