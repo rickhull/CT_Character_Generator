2dafcf01d5f205c02c9ce5766c497c437bf3a04e
Rick Hull <rick.hull@gmail.com>
2017-11-28 06:38:18 +0000

    keep distinct scoped Traveller module

--- lib/traveller/generator.rb
+++ lib/traveller/generator.rb
@@ -1,55 +1,54 @@
 require 'traveller/character'
 require 'traveller/data'
 
-module Traveller::Character::Generator
-  C = Traveller::Character
-  D = Traveller::Data
-
-  def self.character(descr = {})
-    C.new(desc: self.desc.merge(descr),
-          stats: C::Stats.roll)
-  end
-
-  def self.name(gender)
-    case gender.to_s.downcase
-    when 'm', 'male'
-      D.sample('male_names.txt')
-    when 'f', 'female'
-      D.sample('female_names.txt')
-    else
-      raise "unknown gender: #{gender}"
+module Traveller
+  module Generator
+    def self.character(descr = {})
+      Character.new(desc: self.desc.merge(descr),
+                    stats: Character::Stats.roll)
     end
-  end
 
-  def self.gender
-    Traveller.roll(dice: 1) > 3 ? 'M' : 'F'
-  end
+    def self.name(gender)
+      case gender.to_s.downcase
+      when 'm', 'male'
+        Data.sample('male_names.txt')
+      when 'f', 'female'
+        Data.sample('female_names.txt')
+      else
+        raise "unknown gender: #{gender}"
+      end
+    end
 
-  def self.hair(tone: nil, body: nil, color: nil, length: nil)
-    tone ||= D.sample('hair_tone.txt')
-    body ||= D.sample('hair_body.txt')
-    color ||= D.sample('hair_colors.txt')
-    length ||= D.sample('hair_length.txt')
-    [tone, body, color, length].join(' ')
-  end
+    def self.gender
+      Traveller.roll(dice: 1) > 3 ? 'M' : 'F'
+    end
 
-  def self.appearance(hair: nil, skin: nil)
-    hair ||= self.hair
-    skin ||= D.sample('skin_tones.txt')
-    "#{hair} hair with #{skin} skin"
-  end
+    def self.hair(tone: nil, body: nil, color: nil, length: nil)
+      tone ||= Data.sample('hair_tone.txt')
+      body ||= Data.sample('hair_body.txt')
+      color ||= Data.sample('hair_colors.txt')
+      length ||= Data.sample('hair_length.txt')
+      [tone, body, color, length].join(' ')
+    end
 
-  def self.plot
-    D.sample('plots.txt')
-  end
+    def self.appearance(hair: nil, skin: nil)
+      hair ||= self.hair
+      skin ||= Data.sample('skin_tones.txt')
+      "#{hair} hair with #{skin} skin"
+    end
 
-  def self.temperament
-    D.sample('temperaments.txt')
-  end
+    def self.plot
+      Data.sample('plots.txt')
+    end
 
-  def self.desc
-    gender = self.gender
-    C::Description.new(self.name(gender), gender, 18,
-                       self.appearance, self.plot, self.temperament)
+    def self.temperament
+      Data.sample('temperaments.txt')
+    end
+
+    def self.desc
+      gender = self.gender
+      Character::Description.new(self.name(gender), gender, 18,
+                                 self.appearance, self.plot, self.temperament)
+    end
   end
 end
